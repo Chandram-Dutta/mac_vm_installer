1bbadb7b9c1d1da7381e146f02e48baf9a0078ff
Chandram-Dutta <chandramdutta2004@gmail.com>
2021-10-11 11:28:57 +0530

    MInor Update

--- lib/responsive/responsive.dart
+++ lib/responsive/responsive.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 bool isDesktop(BuildContext context) {
   if (MediaQuery.of(context).size.width < 800) {
     return false;
-  } else
+  } else {
     return true;
+  }
 }
