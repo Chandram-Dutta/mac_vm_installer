b9a56388566ac1a0c77a3e5143c76e6538acccaf
Chandram-Dutta <chandramdutta2004@gmail.com>
2021-10-11 11:28:01 +0530

    Major Updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:mac_vm_installer/responsive/responsive.dart';
+import 'package:rive/rive.dart';
+import 'package:yaru/yaru.dart' as yaru;
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +14,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: yaru.lightTheme,
+      darkTheme: yaru.darkTheme,
       home: const MyHomePage(),
     );
   }
@@ -26,9 +28,25 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.black,
-        elevation: 0,
         title: const Text("Linux MacVM Installer"),
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Flexible(
+            flex: 1,
+            child: RiveAnimation.asset("assets/rive/macos.riv"),
+          ),
+          const SizedBox(
+            width: 20,
+          ),
+          Flexible(
+              flex: 2,
+              child: Text(
+                "Welcome to MacVM \nInstaller",
+                style: TextStyle(fontSize: isDesktop(context) ? 48 : 20),
+              ))
+        ],
       ),
     );
   }
